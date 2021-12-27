90ccec79b354261f3cac72b76518492eeb6ddc2d
MartiniMoe <martini.moe@cheex.de>
2021-12-27 18:15:06 +0100

    Add hostname on boot

--- boot.py
+++ boot.py
@@ -1,5 +1,11 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
 import gc
 import webrepl
+import network
+
+sta = network.WLAN(network.STA_IF)
+sta.active(True)
+sta.config(dhcp_hostname='ircometer')
+
 gc.collect()
 webrepl.start()
